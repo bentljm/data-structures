b206afb676380dce618e299043c9e5442e716884
Jeff Bentler <bentljm@gmail.com>
2016-12-14 17:50:01 -0800

    Removed excess spaces

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,33 +5,23 @@ var Queue = function() {
   var storage = {};
   var length = 0;
 
-  // Implement the methods below
-
   someInstance.enqueue = function(value) {
-
     storage[length] = value;
-
     length++;
   };
 
   someInstance.dequeue = function() {
-
     var popped = storage['0'];
-
     var keys = Object.keys(storage);
-
     for (var i = 0; i < keys.length; i++) {
       storage[i] = storage[keys[i + 1]];
     }
-
     length--;
 
     if (length < 0) {
       length = 0;
     }
-
     return popped;
-
   };
 
   someInstance.size = function() {
