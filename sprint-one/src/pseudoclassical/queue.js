67b6cd1598fa8df691cf53589983e070f72f422b
Jeff Bentler <bentljm@gmail.com>
2016-12-17 16:47:09 -0800

    Removed an unnecesary variable name

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,34 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  this.someInstance = {};
-  this.someInstance.storage = {};
-  this.someInstance.length = 0;
+  this.storage = {};
+  this.length = 0;
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.someInstance.storage[this.someInstance.length] = value;
-  this.someInstance.length++;
+  this.storage[this.length] = value;
+  this.length++;
 };
 
 Queue.prototype.dequeue = function() {
-  var popped = this.someInstance.storage['0'];
-  var keys = Object.keys(this.someInstance.storage);
+  var popped = this.storage['0'];
+  var keys = Object.keys(this.storage);
 
   for (var i = 0; i < keys.length; i++) {
-    this.someInstance.storage[i] = this.someInstance.storage[keys[i + 1]];
+    this.storage[i] = this.storage[keys[i + 1]];
   }
-  this.someInstance.length--;
+  this.length--;
 
-  if (this.someInstance.length < 0) {
-    this.someInstance.length = 0;
+  if (this.length < 0) {
+    this.length = 0;
   }
 
   return popped;
 };
 
 Queue.prototype.size = function() {
-  return this.someInstance.length;
+
+  return this.length;
 
 };
 
